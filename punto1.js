51d4111fd76508ab3e02b57680d0e6f03fc08c6f
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-09-07 09:11:27 -0500

    Update punto1.js

--- punto1.js
+++ punto1.js
@@ -1,15 +1,22 @@
 function sueldo(grupoEmpleados = prompt('¿Que cantidad de empleados va a usar?')){
     let nombres = [];
     let sueldos = [];
+    let promedio = 0;
     for (let i = 0; i < grupoEmpleados; i++) {
         let nombre = prompt('Cual es el nombre de su empleado? ');
-        let s = prompt('Cual es el sueldo de este?');
+        let s = Number(prompt('Cual es el sueldo de este?'));
+        promedio += s;
 
         nombres.push(nombre);
         sueldos.push(s);
     }
-    let promedio = sueldos.reduce((acumular, actual)=>{
-        acumular += actual;
-    });
-    // no terminado
+    promedio /= sueldos.length;
+    console.log(promedio);
+    for (let y = 0; y < sueldos.length; y++){
+        if (sueldos[y] > promedio) {
+            alert('El empleado '+nombres[y]+' Gana mas que el promedio')
+        } else {
+            alert(`${nombres[y]} no gana mas del promedio`);
+        }
+    }
 }
